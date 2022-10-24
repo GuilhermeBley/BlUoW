9330fda4d37a53699e768f286781a20125fc8304
GuilhermeBley <guilhermecbley@gmail.com>
2022-10-24 08:49:42 -0300

    add tests and new connection

--- test/BlUoW.Dapper.Tests/DapperUowTests.cs
+++ test/BlUoW.Dapper.Tests/DapperUowTests.cs
@@ -15,11 +15,11 @@ namespace BlUoW.Dapper.Tests;
 
 public class DapperUowTests : DiTestBase
 {
-    private readonly TestRepository _testRepository;
+    private readonly Table1ConnectionRepository _testRepository;
     public DapperUowTests() : base(AddServices)
     {
         _testRepository 
-            = ServiceProvider.CreateScope().ServiceProvider.GetRequiredService<TestRepository>();
+            = ServiceProvider.CreateScope().ServiceProvider.GetRequiredService<Table1ConnectionRepository>();
     }
 
     [Fact]
@@ -61,6 +61,62 @@ public class DapperUowTests : DiTestBase
         }
     }
 
+    [Fact]
+    public async Task Add_WithOutOpenConnection_FailedConnection()
+    {
+        var service = GetSessionestRepository();
+        var table1Repository = service.Table1Repository;
+        var uoW = service.UoW;
+
+        await Assert.ThrowsAnyAsync<ArgumentNullException>(()=>table1Repository.AddAsync(
+            new Table1
+            {
+                Execution = uoW.IdUoW,
+                InsertAt = DateTime.Now,
+                Message = "inserted"
+            }));
+    }
+
+    [Fact]
+    public async Task AddWithTransaction_WithBeginTransaction_SuccessAddAndRemove()
+    {
+        var service = GetSessionestRepository();
+        var table1Repository = service.Table1Repository;
+        var uoW = service.UoW;
+
+        using (uoW.BeginTransactionAsync())
+        {
+            var insert = await table1Repository.AddAsync(
+                new Table1
+                {
+                    Execution = uoW.IdUoW,
+                    InsertAt = DateTime.Now,
+                    Message = "inserted"
+                });
+
+            Assert.NotNull(insert);
+
+            if (insert is null) return;
+
+            var get = await table1Repository.GetByIdOrEmptyAsync(insert.Id);
+
+            Assert.NotNull(get);
+
+            if (get is null) return;
+
+            var delete = await table1Repository.DeleteAsync(get.Id);
+
+            Assert.NotNull(delete);
+
+            if (delete is null) return;
+
+            var getNull = await table1Repository.GetByIdOrEmptyAsync(delete.Id);
+
+            Assert.Null(getNull);
+        }
+    }
+
+
     /// <summary>
     /// Gets a scope service
     /// </summary>
@@ -80,7 +136,7 @@ public class DapperUowTests : DiTestBase
             
             .AddScoped(typeof(Table2Repository))
             .AddScoped(typeof(Table1Repository))
-            .AddScoped(typeof(TestRepository))
+            .AddScoped(typeof(Table1ConnectionRepository))
             
             .AddScoped(typeof(ServiceTest));
     }
--- test/BlUoW.Dapper.Tests/Repositories/TestRepository.cs
+++ test/BlUoW.Dapper.Tests/Repositories/TestRepository.cs
@@ -4,56 +4,57 @@ using Dapper;
 
 namespace BlUoW.Dapper.Tests.Repositories;
 
-internal class TestRepository
+internal class Table1ConnectionRepository : IRepository<Table1, Table1, Guid>
 {
     private readonly IConnectionFactory _connectionFactory;
 
-    public TestRepository(IConnectionFactory connectionFactory)
+    public Table1ConnectionRepository(IConnectionFactory connectionFactory)
     {
         _connectionFactory = connectionFactory;
     }
-
-    public async Task<int> InsertTable1(Table1 table)
-    {
-        return await _connectionFactory.GetNewConnection().ExecuteAsync(
-            "INSERT IGNORE INTO test.table1 (Id, Execution, Message, InsertAt) VALUES (@Id, @Execution, @Message, @InsertAt);",
-            table
-        );
-    }
-
-    public async Task<int> InsertTable2(Table2 table)
-    {
-        return await _connectionFactory.GetNewConnection().ExecuteAsync(
-            "INSERT IGNORE INTO test.table2 (Id, Execution, Message, InsertAt) VALUES (@Id, @Execution, @Message, @InsertAt);",
-            table
-        );
-    }
-
-    public async Task<int> DeleteAllDataTable1()
+    
+    public async Task<int> DeleteAll()
     {
         return await _connectionFactory.GetNewConnection().ExecuteAsync(
             "DELETE FROM test.table1;"
         );
     }
 
-    public async Task<int> DeleteAllDataTable2()
+    public async Task<Table1> AddAsync(Table1 model)
     {
-        return await _connectionFactory.GetNewConnection().ExecuteAsync(
-            "DELETE FROM test.table2;"
+        await _connectionFactory.GetNewConnection().ExecuteAsync(
+            "INSERT IGNORE INTO test.table1 (Id, Execution, Message, InsertAt) VALUES (@Id, @Execution, @Message, @InsertAt);",
+            model
+        );
+
+        return model;
+    }
+
+    public Task<Table1> UpdateAsync(Guid id, Table1 model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Table1?> DeleteAsync(Guid id)
+    {
+        return await _connectionFactory.GetNewConnection().QueryFirstOrDefaultAsync<Table1>(
+            "SELECT Id, Execution, Message, InsertAt FROM test.table1 WHERE Id=@id;" +
+            "DELETE FROM test.table1 WHERE Id=@id;"
+            , new { Id = id }
         );
     }
 
-    public async Task<IEnumerable<Table1>> GetAllTable1()
+    public async Task<Table1?> GetByIdOrEmptyAsync(Guid id)
+    {
+        return await _connectionFactory.GetNewConnection().QueryFirstOrDefaultAsync<Table1>(
+            "SELECT Id, Execution, Message, InsertAt FROM test.table1 WHERE Id=@id;", new { Id = id }
+        );
+    }
+
+    public async Task<IEnumerable<Table1>> GetAllAsync()
     {
         return await _connectionFactory.GetNewConnection().QueryAsync<Table1>(
             "SELECT Id, Execution, Message, InsertAt FROM test.table1;"
-        );
-    }
-
-    public async Task<IEnumerable<Table1>> GetAllTable2()
-    {
-        return await _connectionFactory.GetNewConnection().QueryAsync<Table1>(
-            "SELECT Id, Execution, Message, InsertAt FROM test.table2;"
         );
     }
 }
